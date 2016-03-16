bf5ae735c2645b6ec844fe56251a0f8b313676b0
Tom Coleman <tom@thesnail.org>
2016-03-16 12:17:34 +1100

    Repro better

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
+import { dirnameTest } from 'meteor/tmeasday:dirname-test';
+debugger;
+dirnameTest();
+
+
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
